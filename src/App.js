14818023f8c854ae43b896033e96a7cfe755cf53
Savannaha <savwiley@yahoo.com>
2021-07-17 14:03:35 -0500

    created authentication logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import firebase from "./firebase";
+//import firebaseUI from "./firebase";
 
-function App() {
+const App = () => {
+  const email = useRef();
+  const password = useRef();
+
+  //Changes useRef
+  useEffect(() => {
+    const emailInput = document.querySelector(".emailInput");
+    const pwInput = document.querySelector(".pwInput");
+
+    emailInput.addEventListener("change", () => {
+      email.current = emailInput.value;
+    })
+    pwInput.addEventListener("change", () => {
+      password.current = pwInput.value;
+    })
+  });
+
+  //Btns & Firebase
+  useEffect(() => {
+    const signInBtn = document.querySelector(".signInBtn");
+    const signUpBtn = document.querySelector(".signUpBtn");
+    const signInAnon = document.querySelector(".signInAnon");
+
+    //SIGN IN
+    const signIn = (email, password) => {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+    };
+
+    //SIGN UP
+    const signUp = (email, password) => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in 
+        var user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ..
+      });
+    };
+
+    //SIGN IN ANONYMOUSLY
+    const signAnon = () => {
+      firebase.auth().signInAnonymously()
+      .then(() => {
+        // Signed in..
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+    }
+
+    signInBtn.addEventListener("click", () => {
+      signIn(email.current, password.current);
+    });
+    signUpBtn.addEventListener("click", () => {
+      signUp(email.current, password.current);
+    });
+    signInAnon.addEventListener("click", signAnon);
+  });
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+
+      <div id="firebaseui-auth-container" className="signInSheet">
+        <input type="email" placeholder="Email" className="emailInput" />
+        <input type="password" placeholder="Password" className="pwInput" />
+        <input type="button" value="Sign In" className="signInBtn" />
+        <input type="button" value="Sign Up" className="signUpBtn" />
+
+        <input type="button" value="Sign In Anon" className="signInAnon" />
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+//for info on func 'user already signed in': https://firebase.google.com/docs/auth/web/start
+
+//for info on signing out: https://firebase.google.com/docs/auth/web/password-auth
+
+//for anon info: https://firebase.google.com/docs/auth/web/anonymous-auth
