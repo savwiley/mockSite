22aed4cf8b52d34b418d5ad257e4b0ab8e24cabf
Savannaha <savwiley@yahoo.com>
2021-07-26 13:08:40 -0500

    created new pages

---
+++ src/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/NavBar.js";
+
+const Dashboard = () => {
+
+  return (
+    <div id="dash">
+      <NavBar />
+    </div>
+  )
+};
+
+export default Dashboard;
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+
+  return (
+    <div id="NavBar">
+    </div>
+  )
+};
+
+export default NavBar;
