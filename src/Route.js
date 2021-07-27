cac799ae43b27966db1e5983b76f7004f2db5464
Savannaha <savwiley@yahoo.com>
2021-07-27 12:31:21 -0500

    redirects user on login

--- src/Route.js
+++ src/Route.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { auth } from "./firebase.js";
 import App from "./App.js";
 import Dashboard from "./Dashboard.js";
 import Settings from "./Settings.js";
 
 const Routes = () => {
+  const [signed, setSigned] = useState(false);
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setSigned(true);
+    } else {
+      setSigned(false);
+    }
+  });
 
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" render={() => <App />} />
+        <Route
+          exact
+          path="/"
+          render={() => (signed ? <Dashboard /> : <App />)}
+        />
         <Route exact path="/dashboard" render={() => <Dashboard />} />
         <Route exact path="/settings" render={() => <Settings />} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 };
 
 export default Routes;
