936ab5b678bf19e4ba36d3b81efed37dbe78989e
Savannaha <savwiley@yahoo.com>
2021-07-27 23:29:34 -0500

    set up settings page

---
+++ src/Settings.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/NavBar.js";
+
+const Settings = () => {
+
+  return (
+    <div id="settings">
+    <NavBar />
+    </div>
+  )
+};
+
+export default Settings;
