ae1bd43fdd3ec6ab86d63933c66cb45bab2620d7
Savannaha <savwiley@yahoo.com>
2021-08-07 13:50:21 -0500

    refactored

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,118 +1,61 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState } from "react";
+import { SignInSheet, SignInControls } from "./styled.js";
+import { SignIn, SignUp } from "./screen.js";
 import { auth } from "../firebase.js";
 
 const Home = () => {
-  const email = useRef();
-  const password = useRef();
+  const [signUpScreen, setSignUpScreen] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-  //changes signIn/signUp screens
-  useEffect(() => {
-    //selectors
-    const upText = document.querySelector("#upP");
-    const inText = document.querySelector("#inP");
-    const signInBtn = document.querySelector(".signInBtn");
-    const signUpBtn = document.querySelector(".signUpBtn");
-
-    upText.addEventListener("click", () => {
-      inText.style.display = "block";
-      signUpBtn.style.display = "block";
-      signInBtn.style.display = "none";
-      upText.style.display = "none";
-    });
-
-    inText.addEventListener("click", () => {
-      signInBtn.style.display = "block";
-      upText.style.display = "block";
-      signUpBtn.style.display = "none";
-      inText.style.display = "none";
-    });
-  });
-
-  //Changes useRef
-  useEffect(() => {
-    const emailInput = document.querySelector(".emailInput");
-    const pwInput = document.querySelector(".pwInput");
-
-    emailInput.addEventListener("change", () => {
-      email.current = emailInput.value;
-    });
-    pwInput.addEventListener("change", () => {
-      password.current = pwInput.value;
-    });
-  });
-
-  //Btns & Firebase
-  useEffect(() => {
-    const signInBtn = document.querySelector(".signInBtn");
-    const signUpBtn = document.querySelector(".signUpBtn");
-    const signInAnon = document.querySelector(".signInAnon");
-
-    //SIGN IN
-    const signIn = (email, password) => {
-      auth
-        .signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-          const user = userCredential.user;
-        })
-        .catch((error) => {
-          console.log(error.code);
-          alert(error.message);
-        });
-    };
-
-    //SIGN UP
-    const signUp = (email, password) => {
-      auth
-        .createUserWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-          const user = userCredential.user;
-        })
-        .catch((error) => {
-          console.log(error.code);
-          alert(error.message);
-        });
-    };
-
-    //SIGN IN ANONYMOUSLY
-    const signAnon = () => {
-      auth
-        .signInAnonymously()
-        .then(() => {
-          // Signed in..
-        })
-        .catch((error) => {
-          console.log(error.code);
-          alert(error.message);
-        });
-    };
-
-    signInBtn.addEventListener("click", () => {
-      signIn(email.current, password.current);
-    });
-    signUpBtn.addEventListener("click", () => {
-      signUp(email.current, password.current);
-    });
-    signInAnon.addEventListener("click", signAnon);
-  });
+  const signAnon = () => {
+    auth
+      .signInAnonymously()
+      .then(() => {
+        // Signed in..
+      })
+      .catch((error) => {
+        console.log(error.code);
+        alert(error.message);
+      });
+  };
 
   return (
-    <div className="home">
-      <div id="firebaseui-auth-container" className="signInSheet">
-        <h1>Notagram</h1>
+    <SignInSheet>
+      <h1>Notagram</h1>
 
-        <input type="email" placeholder="Email" className="emailInput" />
-        <input type="password" placeholder="Password" className="pwInput" />
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={() => {
+          setEmail(this.value);
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={() => {
+          setPassword(this.value);
+        }}
+      />
 
-        <div>
-          <input type="button" value="Sign In" className="signInBtn" />
-          <span id="inP">Sign In</span>
-          <span id="upP">Sign Up</span>
-          <input type="button" value="Sign Up" className="signUpBtn" />
-        </div>
+      <SignInControls>
+        {!signUpScreen && (
+          <SignIn creds={[email, password]} setShown={setSignUpScreen} />
+        )}
+        {signUpScreen && (
+          <SignUp creds={[email, password]} setShown={setSignUpScreen} />
+        )}
+      </SignInControls>
 
-        <input type="button" value="Enter Anonymously" className="signInAnon" />
-      </div>
-    </div>
+      <input
+        type="button"
+        value="Enter Anonymously"
+        onClick={() => {
+          signAnon();
+        }}
+      />
+    </SignInSheet>
   );
 };
 
---
+++ src/components/Home/screen.js
@@ -0,0 +1,74 @@
+import { SwitchScreen } from "./styled.js";
+import { auth } from "../firebase.js";
+
+//sign in screen
+export const SignIn = (props) => {
+  const { setShown, creds } = props;
+
+  const signIn = (email, password) => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        //const user = userCredential.user;
+      })
+      .catch((error) => {
+        console.log(error.code);
+        alert(error.message);
+      });
+  };
+
+  return (
+    <>
+      <input
+        type="button"
+        value="Sign In"
+        onClick={() => {
+          signIn(creds[0], creds[1]);
+        }}
+      />
+      <SwitchScreen
+        onClick={() => {
+          setShown(true);
+        }}
+      >
+        Sign Up
+      </SwitchScreen>
+    </>
+  );
+};
+
+//sign up screen
+export const SignUp = (props) => {
+  const { setShown, creds } = props;
+
+  const signUp = (email, password) => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        //const user = userCredential.user;
+      })
+      .catch((error) => {
+        console.log(error.code);
+        alert(error.message);
+      });
+  };
+
+  return (
+    <>
+      <SwitchScreen
+        onClick={() => {
+          setShown(false);
+        }}
+      >
+        Sign In
+      </SwitchScreen>
+      <input
+        type="button"
+        value="Sign Up"
+        onClick={() => {
+          signUp(creds[0], creds[1]);
+        }}
+      />
+    </>
+  );
+};
