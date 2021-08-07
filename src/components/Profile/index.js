9d532215f5565bad34d1dcad4b01e5d2de34e14d
Savannaha <savwiley@yahoo.com>
2021-08-07 16:33:30 -0500

    made Profile folder

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,13 +1,24 @@
-import React from "react";
 //import { useParams } from "react-router-dom";
+import NavBar from "../NavBar";
 
 const Profile = () => {
   //const displayName = useParams();
 
   return(
     <>
+    <NavBar />
     </>
   )
 };
 
 export default Profile;
+
+//https://firebase.google.com/docs/auth/admin/manage-users
+
+/**
+ * Can't retrieve data of other users. Solution:
+ * 
+ * The PROFILE is only for the signed-in user and displays their posts.
+ * 
+ * Each post will hold the user's displayName & image as metadata. Comments & likes will somehow be included. 
+ */
