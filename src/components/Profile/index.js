d2510585f338b193faee5a8c4b1d31407878da4d
Savannaha <savwiley@yahoo.com>
2021-08-21 16:23:38 -0500

    set up useParams & firestore promise

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,14 +1,54 @@
-import React from "react";
-//import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import NavBar from "../NavBar";
+import { ProfilePosts } from "./postComponents/profilePosts";
+import { ProfileHeader } from "./styled";
+import firebase from "../firebase";
 
 const Profile = () => {
-  //const displayName = useParams();
+  const [profilePosts, setProfilePosts] = useState();
+  const [makeProfilePosts, setMakeProfilePosts] = useState();
+  const [profilePic, setProfilePic] = useState();
+  const [readyProfile, setReadyProfile] = useState(false);
+  const id = useParams();
+
+  async function callAsync() {
+    const postRef = firebase
+      .firestore()
+      .collection("posts")
+      .where("postOwner", "==", id.displayName)
+      .orderBy("date", "desc");
+    const eachPost = await postRef.get();
+    setProfilePosts(eachPost);
+  }
+
+  useEffect(() => {
+    if (!profilePosts) {
+      callAsync();
+    } else {
+      const postsArr = [];
+      profilePosts.forEach((e) => {
+        postsArr.push(e.data());
+        !profilePic && setProfilePic(e.data().ownerPic);
+      });
+      setMakeProfilePosts(postsArr);
+      setReadyProfile(true);
+    }
+  }, [profilePosts]);
 
   return (
     <>
       <NavBar />
+      <ProfileHeader>
+        <img src={profilePic} alt="It's them!" /> 
+        {id.displayName}
+      </ProfileHeader>
 
+      {readyProfile ? (
+        <ProfilePosts posts={makeProfilePosts} />
+      ) : (
+        "There's nothing here yet."
+      )}
     </>
   );
 };
@@ -16,3 +56,5 @@ const Profile = () => {
 export default Profile;
 
 //https://firebase.google.com/docs/auth/admin/manage-users
+
+//useParams creates an object => {displayName: "SavWileyy"}
