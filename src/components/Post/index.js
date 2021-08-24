9f1ef599686acafc4ceca3f706c498b8d8d30fb3
Savannaha <savwiley@yahoo.com>
2021-08-24 17:15:38 -0500

    call firestore for post information

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,7 +1,38 @@
-import React, { useParams } from "react";
+import React, { useState, useEffect, useParams } from "react";
+import firebase from "../firebase";
 
 const PostPage = () => {
+  const [ allPostInfo, setAllPostInfo ] = useState();
+  const [ makePostInfo, setMakePostInfo ] = useState();
+  const [ readyPost, setReadyPost ] = useState(false);
+  const { displayName, image } = useParams();
 
+  async function callAsync() {
+    const postRef = firebase
+      .firestore()
+      .collection("posts")
+      .where("postOwner", "==", `${displayName.displayName}`);
+    const eachPost = await postRef.get();
+    setAllPostInfo(eachPost);
+  }
+
+  useEffect(() => {
+    if (!allPostInfo) {
+      callAsync();
+    } else {
+      const postsArr = [];
+      postsArr.push(allPostInfo.filter(e => {
+        e.data().postPic === image.postPic;
+      }));
+      setMakePostInfo(postsArr);
+      setReadyPost(true);
+    }
+  }, [allPostInfo]);
+
+  return(
+    <>
+    </>
+  )
 };
 
 export default PostPage;
