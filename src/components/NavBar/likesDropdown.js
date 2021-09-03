7c5e953b2c088bb1b50d2c7ede745fe1c5ffbce5
Savannaha <savwiley@yahoo.com>
2021-09-03 14:12:46 -0500

    fix props import & add links

--- src/components/NavBar/likesDropdown.js
+++ src/components/NavBar/likesDropdown.js
@@ -1,24 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { LikeDropStyle, EachLike, PreviewLikePic } from "./styled";
 
 const LikeDropDown = (props) => {
-  const likedPosts = props;
+  const { likedPosts } = props;
+
 
   return (
     <LikeDropStyle>
       <b>Liked Posts</b>
-      {likedPosts.forEach(e => {
+      {likedPosts.map(e => (
         <EachLike key={e.date}>
-          <img src={e.ownerPic} />
+          <Link to={`/${displayName}`} title="Profile">
+            <img src={e.ownerPic} />
+          </Link>
           <span>
             You liked <b>{e.postOwner}</b>&apos;s post!
           </span>
           <PreviewLikePic>
-            <img src={e.postPic} />
+            <Link to={`/${e.postOwner}/${e.date.seconds}`} title="Post">
+              <img src={e.postPic} />
+            </Link>
           </PreviewLikePic>
         </EachLike>
-      })}
+      ))}
     </LikeDropStyle>
   );
 };
