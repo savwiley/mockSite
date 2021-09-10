4745c1bc06f778023507efec45d1ddbd4ed5c125
Savannaha <savwiley@yahoo.com>
2021-09-10 15:00:12 -0500

    add margins & padding

--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 
 export const FooterComp = styled.div`
-  color: rgb(199,199,199);
+  color: rgb(199, 199, 199);
   text-align: center;
   font-size: 11px;
   display: block;
-  border-top: 1px solid rgb(199,199,199);
+  border-top: 1px solid rgb(199, 199, 199);
   width: 50%;
-  margin: auto;
+  margin: 50px auto 0;
+  padding: 20px 0;
+  cursor: default;
   a {
-    color: rgb(199,199,199);
+    color: rgb(199, 199, 199);
     text-decoration: none;
   }
 `;
