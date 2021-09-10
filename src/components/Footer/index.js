d62168648f3aacbfa46a8bbadf1a0fab370422e1
Savannaha <savwiley@yahoo.com>
2021-09-10 14:53:35 -0500

    create Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FooterComp } from "./styled";
+
+const Footer = () => {
+  return (
+    <FooterComp>
+      <a href="https://github.com/savwiley/mockSite/blob/master/README.md">About</a> ⋅ <a href="https://github.com/savwiley/mockSite/issues">Bug Report</a> ⋅ <a href="https://github.com/savwiley/mockSite">Code</a> ⋅ <a href="https://savwiley.com/">Creator's Portfolio</a>
+    </FooterComp>
+  )
+};
+
+export default Footer;
---
+++ src/components/Footer/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const FooterComp = styled.div`
+  color: rgb(199,199,199);
+  text-align: center;
+  font-size: 11px;
+  display: block;
+  border-top: 1px solid rgb(199,199,199);
+  width: 50%;
+  margin: auto;
+  a {
+    color: rgb(199,199,199);
+    text-decoration: none;
+  }
+`;
