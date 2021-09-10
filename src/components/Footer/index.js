b18524efcfb8295291f698df23022ea6d6e59386
Savannaha <savwiley@yahoo.com>
2021-09-10 14:59:57 -0500

    format code

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,14 @@ import { FooterComp } from "./styled";
 const Footer = () => {
   return (
     <FooterComp>
-      <a href="https://github.com/savwiley/mockSite/blob/master/README.md">About</a> ⋅ <a href="https://github.com/savwiley/mockSite/issues">Bug Report</a> ⋅ <a href="https://github.com/savwiley/mockSite">Code</a> ⋅ <a href="https://savwiley.com/">Creator's Portfolio</a>
+      <a href="https://github.com/savwiley/mockSite/blob/master/README.md">
+        About
+      </a>{" "}
+      ⋅ <a href="https://github.com/savwiley/mockSite/issues">Bug Report</a> ⋅{" "}
+      <a href="https://github.com/savwiley/mockSite">Code</a> ⋅{" "}
+      <a href="https://savwiley.com/">Creator&apos;s Portfolio</a>
     </FooterComp>
-  )
+  );
 };
 
 export default Footer;
