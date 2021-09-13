e9e8d3c07d888a01ee1a830562509d10d226b898
Savannaha <savwiley@yahoo.com>
2021-09-13 13:54:14 -0500

    add animated gradient to smaller Loading icon

--- src/components/Loading/styled.js
+++ src/components/Loading/styled.js
@@ -12,6 +12,21 @@ const colorChange = keyframes`
   }
 `;
 
+const radialChange = keyframes`
+  0% {
+    color: radial-gradient(circle, rgba(255,222,0,1) 0%, rgba(177,0,131,1) 50%, rgba(21,54,157,1) 100%);
+  }
+  33% {
+    color: radial-gradient(circle, rgba(177,0,131,1) 0%, rgba(21,54,157,1) 50%, rgba(255,222,1) 100%);
+  }
+  66% {
+    color: radial-gradient(circle, rgba(21,54,157,1) 0%, rgba(255,222,1) 50%, rgba(177,0,131,1) 100%);
+  }
+  100% {
+    color: radial-gradient(circle, rgba(255,222,0,1) 0%, rgba(177,0,131,1) 50%, rgba(21,54,157,1) 100%);
+  }
+`;
+
 export const PageLoading = styled.div`
   background: #fff;
   position: fixed;
@@ -29,4 +44,21 @@ export const PageLoading = styled.div`
       animation: ${colorChange} 2s linear infinite;
     }
   }
+`;
+
+export const SmallLoading = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 20;
+  display: flex;
+  .icon {
+    width: fit-content;
+    font-size: 6rem;
+    margin: auto;
+    path {
+      animation: ${radialChange} 2s linear infinite;
+    }
 `;
