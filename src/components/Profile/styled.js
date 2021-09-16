68bb12ba1bb9d59cfb4800c125d16aad3950216a
Savannaha <savwiley@yahoo.com>
2021-09-16 11:41:34 -0500

    style post overlay & links

--- src/components/Profile/styled.js
+++ src/components/Profile/styled.js
@@ -27,6 +27,9 @@ export const PostSection = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   margin: 10px 0;
+  a {
+    text-decoration: none;
+  }
 `;
 
 export const PostSquare = styled.div`
@@ -35,4 +38,20 @@ export const PostSquare = styled.div`
   overflow: hidden;
   margin: 5px;
   background: center/cover no-repeat url(${(props) => props.background});
+`;
+
+export const PostOverlay = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.2);
+  color: #fff;
+  font-weight: bold;
+  font-size: 28px;
+  path {
+    color: #fff;
+  }
 `;
