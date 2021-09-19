13c340f6f93985b4f4f447efe279c9b94e68b866
Savannaha <savwiley@yahoo.com>
2021-09-18 21:08:11 -0500

    format code

--- src/components/NavBar/searchDropdown.js
+++ src/components/NavBar/searchDropdown.js
@@ -2,14 +2,14 @@ import React from "react";
 import { SearchDropStyle, SearchHead } from "./styled";
 
 const SearchDrop = () => {
-  return(
+  return (
     <SearchDropStyle>
       <SearchHead>
         <h4>Recent</h4>
         <span>Clear All</span>
       </SearchHead>
     </SearchDropStyle>
-  )
+  );
 };
 
-export default SearchDrop
+export default SearchDrop;
