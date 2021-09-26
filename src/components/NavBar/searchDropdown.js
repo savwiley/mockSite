dd457235e95997d5a570cdf55c9c0d23e4a384c6
Savannaha <savwiley@yahoo.com>
2021-09-25 19:15:57 -0500

    call firebase for search criteria

--- src/components/NavBar/searchDropdown.js
+++ src/components/NavBar/searchDropdown.js
@@ -1,8 +1,39 @@
-import React from "react";
-import { SearchDropStyle, SearchHead } from "./styled";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { SearchDropStyle, SearchHead, SearchResults } from "./styled";
 
 const SearchDrop = (props) => {
-  const {criteria} = props; //value from search input field
+  const { criteria, firebase } = props; //value from search input field
+  const [oldValue, setOldValue] = useState("");
+  const [posts, setPosts] = useState();
+  const [makePosts, setMakePosts] = useState();
+
+  async function callAsync() {
+    const postRef = firebase
+      .firestore()
+      .collection("posts")
+      .where("postMessage", "contains", `${criteria}`);
+      //.orderBy("date", "desc");
+    const eachPost = await postRef.get();
+    setPosts(eachPost);
+  }
+
+  /**You can't search through firebase by regular means. Recommended to try elasticsearch. */
+  //https://www.elastic.co/elasticsearch/
+  //https://firebase.googleblog.com/2014/01/queries-part-2-advanced-searches-with.html
+
+  useEffect(() => {
+    if (criteria !== oldValue) {
+      callAsync();
+    } else {
+      const postsArr = [];
+      posts.forEach((e) => {
+        postsArr.push(e.data());
+      });
+      setMakePosts(postsArr);
+      setOldValue(criteria);
+    }
+  }, [posts, criteria, oldValue]);
 
   return (
     <SearchDropStyle>
@@ -10,11 +41,24 @@ const SearchDrop = (props) => {
         <h4>Recent</h4>
         <span>Clear All</span>
       </SearchHead>
+
+      <SearchResults>
+        {makePosts && makePosts.map(e => (
+          <div key={e.date}>
+            {e.postMessage}
+          </div>
+        ))}
+      </SearchResults>
     </SearchDropStyle>
   );
 };
 
 export default SearchDrop;
+
+SearchDrop.propTypes = {
+  criteria: PropTypes.string,
+  firebase: PropTypes.PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+};
 
 /**i want this to search for user display names
  * 
