d8e08d099b421e026b8bcefd79d2044f860cb8e7
Savannaha <savwiley@yahoo.com>
2021-09-26 18:09:46 -0500

    make Clear All click also clear the input value

--- src/components/NavBar/searchDropdown.js
+++ src/components/NavBar/searchDropdown.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { SearchDropStyle, SearchHead, SearchResults } from "./styled";
 
 const SearchDrop = (props) => {
-  const { criteria, firebase, clear } = props; //value from search input field
+  const { criteria, firebase, clear, inputValue } = props;
   const [oldValue, setOldValue] = useState("");
   const [posts, setPosts] = useState();
   const [makePosts, setMakePosts] = useState();
@@ -31,9 +31,8 @@ const SearchDrop = (props) => {
         dataMessage.includes(searching) && postsArr.push(e.data());
         dataOwner.includes(searching) && userArr.push(e.data().postOwner);
       });
-      const uniqueUsers = [...new Set(userArr)];
       setMakePosts(postsArr);
-      setMakeUsers(uniqueUsers);
+      setMakeUsers([...new Set(userArr)]);
     }
   }, [criteria, oldValue, posts]);
 
@@ -45,6 +44,7 @@ const SearchDrop = (props) => {
           onMouseDown={(e) => {
             e.preventDefault();
             clear();
+            inputValue("");
           }}
         >
           Clear All
@@ -65,12 +65,6 @@ export default SearchDrop;
 SearchDrop.propTypes = {
   criteria: PropTypes.string,
   clear: PropTypes.func,
+  inputValue: PropTypes.func,
   firebase: PropTypes.PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
 };
-
-/**i want this to search for user display names
- *
- * https://stackoverflow.com/questions/47778294/firebase-check-if-user-exists-by-display-name
- *
- * also search by post messages
- */
