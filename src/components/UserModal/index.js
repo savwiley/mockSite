4bdaf1cb0aec285ef54a6001b2f166cbc41006d4
Savannaha <savwiley@yahoo.com>
2021-10-18 17:33:04 -0500

    add logic to call firebase for posts

--- src/components/UserModal/index.js
+++ src/components/UserModal/index.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import firebase from "../firebase";
 
 const UserModal = () => {
+  const [posts, setPosts] = useState();
+
+  async function callAsync() {
+    const postRef = firebase
+      .firestore()
+      .collection("posts")
+      .orderBy("date", "desc");
+    const eachPost = await postRef.get();
+    setPosts(eachPost);
+  };
+
+  useEffect(() => {
+    if (!posts) {
+      callAsync();
+    } else {
+      //save posts that contain same owner with icon
+    }
+  });
+
   return (
     <>
     </>
