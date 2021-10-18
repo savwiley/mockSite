e10acc3ad112b3f4f6999d9ba6bae0e750c33f31
Savannaha <savwiley@yahoo.com>
2021-10-17 20:31:58 -0500

    add modal component

--- src/components/UserModal/index.js
+++ src/components/UserModal/index.js
@@ -1,1 +1,14 @@
 import React from "react";
+
+const UserModal = () => {
+  return (
+    <>
+    </>
+  )
+};
+
+export default UserModal;
+
+//a pop-up that appears when icons are hovered over
+
+//contains icon, name, 3 most recent user posts
