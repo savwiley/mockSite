bff5a10f85381e9a4001fdafd777c4f3e225226d
Savannaha <savwiley@yahoo.com>
2021-10-21 20:18:07 -0500

    create style for Modal component

--- src/components/UserModal/styled.js
+++ src/components/UserModal/styled.js
@@ -1,1 +1,10 @@
 import styled from "styled-components";
+
+export const Modal = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  border-radius: 8px;
+  box-shadow: 0 0 8px var(--border);
+  overflow: hidden;
+`;
