6623e7d41b7e1c948ffda71f8775db6725fde6a7
Savannaha <savwiley@yahoo.com>
2021-10-22 14:37:49 -0500

    stylize profile info

--- src/components/UserModal/styled.js
+++ src/components/UserModal/styled.js
@@ -8,3 +8,21 @@ export const Modal = styled.div`
   box-shadow: 0 0 8px var(--border);
   overflow: hidden;
 `;
+
+export const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid var(--border);
+  div {
+    background: center/cover no-repeat url(${props => props.profilePic});
+    width: 75px;
+    height: 75px;
+    border-radius: 50px;
+    margin-right: 10px;
+  }
+  span {
+    font-weight: bold;
+  }
+`;
