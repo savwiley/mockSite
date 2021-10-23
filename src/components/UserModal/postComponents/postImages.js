c341c19fe0cf14a4d39e3a88353612a7d16d5845
Savannaha <savwiley@yahoo.com>
2021-10-23 13:52:37 -0500

    add component to show posts in UserModal

---
+++ src/components/UserModal/postComponents/postImages.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { } from "../styled.js";
+
+const UserModalPosts = (props) => {
+  const { posts } = props;
+
+  return(
+    <>
+    </>
+  )
+};
+
+export default UserModalPosts;
