89b5e91db409bf8d98424720a5c9df9955d3bcbf
Savannaha <savwiley@yahoo.com>
2021-10-24 21:17:15 -0500

    loop through posts for modal

--- src/components/UserModal/postComponents/postImages.js
+++ src/components/UserModal/postComponents/postImages.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { } from "../styled.js";
+import { PostHolder } from "../styled.js";
 
 const UserModalPosts = (props) => {
   const { posts } = props;
 
   return(
     <>
+      {posts.map(e => (
+        <PostHolder key={e.date} background={e.postPic}>
+
+        </PostHolder>
+      ))}
     </>
   )
 };
