a310bb0729d3fd4ee2e3e67592843d698771d15e
Savannaha <savwiley@yahoo.com>
2021-11-03 23:00:09 -0500

    add user modal to dashboard posts

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -3,12 +3,15 @@ import NavBar from "../NavBar";
 import Footer from "../Footer";
 import { ModalLoading } from "../Loading";
 import PostBoard from "./postComponents/posts";
+import UserModal from "../UserModal";
 import firebase from "../firebase";
 
 const Dashboard = () => {
   const [posts, setPosts] = useState();
   const [makePosts, setMakePosts] = useState();
   const [ready, setReady] = useState(false);
+  const [userRead, setUserReady] = useState(false);
+  //i need to grab display names somehow
 
   async function callAsync() {
     const postRef = firebase
