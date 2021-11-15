bf181e134afb1d804522bb4991198ee4ce39c589
Savannaha <savwiley@yahoo.com>
2021-11-15 14:15:14 -0600

    add positioning & adjust sizes

--- src/components/UserModal/styled.js
+++ src/components/UserModal/styled.js
@@ -3,11 +3,17 @@ import styled from "styled-components";
 export const Modal = styled.div`
   display: flex;
   flex-direction: column;
+  position: absolute;
+  left: 65%;
+  top: 20%;
+  width: 200px;
   background: #fff;
   border-radius: 8px;
   box-shadow: 0 0 8px var(--border);
   overflow: hidden;
+  z-index: 2;
 `;
+//see if you can track the mouse for positioning
 
 export const ProfileInfo = styled.div`
   display: flex;
@@ -15,10 +21,11 @@ export const ProfileInfo = styled.div`
   align-items: center;
   justify-content: center;
   border-bottom: 1px solid var(--border);
+  padding: 10px 5px;
   div {
     background: center/cover no-repeat url(${props => props.profilePic});
-    width: 75px;
-    height: 75px;
+    width: 50px;
+    height: 50px;
     border-radius: 50px;
     margin-right: 10px;
   }
@@ -30,10 +37,11 @@ export const ProfileInfo = styled.div`
 export const PostImages = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
 `;
 
 export const PostHolder = styled.div`
-  width: 20px;
-  height: 20px;
-  background: url(${props => props.background});
+  width: 70px;
+  height: 70px;
+  background: center/cover no-repeat url(${props => props.background});
 `;
