67bedace8075863ed872ab51c2c0259024211656
Savannaha <savwiley@yahoo.com>
2021-12-06 21:11:41 -0600

    create navbar menu for mobile use

---
+++ src/components/NavBar/mobileMenu.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  RiHome2Line,
+  RiAddBoxLine,
+  RiUser3Line,
+  RiSettings3Fill,
+} from "react-icons/ri";
+import { IoHeartOutline } from "react-icons/io5";
+import { auth } from "../firebase";
+import /** */ "./styled";
+
+const MobileMenu = () => {
+  return <></>;
+};
+
+export default MobileMenu;
