6dd488e25e6d2c6fef6c42fcdb1939f21f776431
Savannaha <savwiley@yahoo.com>
2021-12-08 18:35:37 -0600

    add icon on hover over profile image

--- src/components/Settings/styled.js
+++ src/components/Settings/styled.js
@@ -14,7 +14,8 @@ export const SettingsTop = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  margin: 50px auto 10px;
+  justify-content: center;
+  margin: 0 auto -50px;
   font-size: 20px;
   width: 0;
 `;
@@ -22,17 +23,46 @@ export const SettingsTop = styled.div`
 export const SettingsPic = styled.div`
   width: 60px;
   padding-right: 10px;
-  div {
+  margin-bottom: 50px;
+  font-size: 40px;
+  .icon {
+    fill: transparent;
     width: 50px;
     height: 50px;
     border-radius: 50px;
     background: center/cover no-repeat url(${(props) => props.picture});
+    margin-bottom: 50px;
+    &:hover {
+      opacity: 1;
+      fill: rgba(250, 250, 250, 0.8);
+      background: linear-gradient(90deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+        center/cover no-repeat url(${(props) => props.picture});
+    }
+  }
+  input {
+    opacity: 0;
+    font-size: 12px !important;
+    cursor: pointer;
+    height: 40px;
+    width: 40px;
+    position: relative;
+    top: 50px;
+    outline: none;
+    ::file-selector-button {
+      display: none;
+    }
+    &:hover ~ .icon {
+      opacity: 1;
+      fill: rgba(250, 250, 250, 0.4);
+      background: linear-gradient(90deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+        center/cover no-repeat url(${(props) => props.picture});
+    }
   }
 `;
 
 export const Form = styled.div`
   width: 300px;
-  margin: 50px auto 0;
+  margin: 80px auto 0;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
@@ -42,6 +72,7 @@ export const Form = styled.div`
     flex-direction: row;
     width: 100%;
     justify-content: space-around;
+    align-items: center;
   }
   label {
     text-align: right;
@@ -51,11 +82,18 @@ export const Form = styled.div`
   }
   input {
     width: 100%;
+    margin: 10px 0;
     &[type="button"] {
+      color: #eee;
       width: fit-content;
+      margin: 10px 0 20px;
       &:hover {
-        color: black;
+        color: #fff;
       }
     }
+  }
+  #delBtn {
+    font-size: 20px;
+    margin-top: 40px;
   }
 `;
